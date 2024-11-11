2216741b3d5d52703277fa6cfe9660439827557b
Azat Rakhmatullin <zzzzzz.yyyyyy.@zzzz.com>
2024-11-11 08:02:08 +0300

    feature: add new values in models

--- app/models/Temperature.cs
+++ app/models/Temperature.cs
@@ -11,8 +11,12 @@ namespace WeatherApplication.app.models
     {
         public float temp;
         public float feels_like;
-
         public float temp_min;
         public float temp_max;
+        public int pressure;
+        public int sea_level;
+        public int grnd_level;
+        public int humidity;
+        public float temp_kf;
     }
 }
